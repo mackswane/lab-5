08d5b9420fc6e1ab6016c1ee748ae5c8d79e78fc
NiserAk <35601403+NiserAk@users.noreply.github.com>
2018-02-09 23:29:03 +0530

    Update 1.cpp

--- 1.cpp
+++ 1.cpp
@@ -2,44 +2,7 @@
 #include<iostream>
 #include<iostream>
 using namespace std;
-// declare the class for node
-class Node{ public:
-	int data;
-	Node*next;
-};
-//class declaration for LinkedList
-class LinkedList{
-public:
-    Node *head;
-    Node *tail;
-    int n;//initialization size of LinkedList
 
-//setting the initial size of the LinkedList
-    LinkedList(){
-    head=NULL;
-    tail=NULL;
-    n=0;
-}
-//inserting node in the end
-    void Insert(int x){
-        Node *temp=new Node;
-        temp->data=x;
-        ++n;
-        //checking if the LinkedList is empty
-        if (head==NULL){
-            head=temp;
-            tail=temp;
-
-        }
-        else{
-
-            tail->next=temp;
-            tail=temp;
-
-        }
-         tail->next=head;
-    }
-};
 //declare class for sorting array and linked list
 class bubbleshort
 {
@@ -57,32 +20,6 @@ for(int i=0; i<size; i++){
 	}
 	return p;
 }
-// declare the function for sorting the linked list
-Node* sorting ( Node*head)
-{
-  Node * temphead = head;
-  Node * tempnode = NULL;
-int counter = 0;
-while (temphead)
-{
-temphead = temphead->next;
-counter++;
-}
-for (int i=0; i<counter; i++)
-{
-for (int j=0; j<counter-i; j++)
-{
-if (temphead->data > temphead->next->data)
-{
-tempnode = temphead;
-temphead = temphead->next;
-temphead->next = tempnode;
-}
-}
-}
-return temphead;
-}
-
 };
 int main()
 {
@@ -102,15 +39,6 @@ int main()
 	{
 		cout<<*(temp+i)<<" ";
 	}
-	LinkedList l;
-	l.Insert(9);
-	l.Insert(8);
-	l.Insert(7);
-	l.Insert(6);
-	cout<<"For linked list \n";
-	Node*lsort;
-	lsort=b.sorting (l.head);
-	while(lsort!=NULL){ cout<<lsort->data<<endl;
-	}
+	
 	return 0;
 }
